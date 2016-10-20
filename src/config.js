7b5ba946b80fb18413ccd0c5c02ad79728893a5e
Casey Young <elementcy@gmail.com>
2016-10-20 14:09:20 -0500

    Removed Slack Token
    oops!

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const config = {
   ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   PROXY_URI: process.env.PROXY_URI,
-  SLACK_TOKEN: 'xoxb-93766701408-UCQS7tM2BeWydIJCcgiQy3dQ', //process.env.SLACK_TOKEN,
+  SLACK_TOKEN: process.env.SLACK_TOKEN,
   ICON_EMOJI: ':space_invader:'
 }
 
